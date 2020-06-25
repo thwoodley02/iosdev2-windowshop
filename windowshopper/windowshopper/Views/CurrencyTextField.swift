4b44236ae0d9676cea15b958c0a62aa7a7b3c088
Tom <thwoodley@gmail.com>
2020-06-25 12:07:47 +0100

    End of Session 5

--- windowshopper/windowshopper/Views/CurrencyTextField.swift
+++ windowshopper/windowshopper/Views/CurrencyTextField.swift
@@ -11,6 +11,21 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height/2 - size/2), width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.6525239944, green: 0.6673359871, blue: 0.660811007, alpha: 0.7011168574)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -24,7 +39,7 @@ class CurrencyTextField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2524757923)
         layer.cornerRadius = 5.0
         textAlignment = .center
-        
+        clipsToBounds = true
         if placeholder == nil {
             placeholder = " "
         }
